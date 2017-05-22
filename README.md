0c8882829c0400e4ba1a70bc9fd357ae4e6b5351
ManWithShotgun <u3n3ter7@mail.ru>
2017-05-22 23:46:02 +0300

    bad cxf-soap impl osgi
    need give it a clean

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Запуск tomcat через плагин maven -> cxf-soap -> tomcat: run
 
 tomcat: http://localhost:9999/cxf-soap/cxf/soap?wsdl
+
+
+cxf-soap(osgi karaf):
+http://localhost:8181/system/console/bundles
+http://localhost:8181/cxf/  ----- http bundle (list cxf services)
+
+install -s mvn:ru.ilia/osgi-blueprint/1.0-SNAPSHOT
+
+kar:install mvn:ru.ilia/osgi-feature/1.0-SNAPSHOT
+mvn:ru.ilia/osgi-feature/1.0-SNAPSHOT
--- cxf-soap/src/main/java/ru/ilia/soap/webservice/AverageServiceImpl.java
+++ cxf-soap/src/main/java/ru/ilia/soap/webservice/AverageServiceImpl.java
@@ -1,16 +1,15 @@
 package ru.ilia.soap.webservice;
 
-import net.webservicex.ArrayOfDouble;
-import net.webservicex.Statistics;
-import net.webservicex.StatisticsSoap;
+//import net.webservicex.ArrayOfDouble;
+//import net.webservicex.Statistics;
+//import net.webservicex.StatisticsSoap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import ru.ilia.soap.client.SoapClient;
+//import ru.ilia.soap.client.SoapClient;
 import ru.ilia.soap.exception.BaseException;
 import ru.ilia.soap.exception.SumLessZeroException;
 
-import javax.ws.rs.core.Response;
+import javax.jws.WebService;
 import javax.xml.ws.Holder;
 import java.util.List;
 
@@ -18,7 +17,7 @@ import java.util.List;
  * Created by ILIA on 09.04.2017.
  */
 
-@Service("AverageService")
+@WebService()
 public class AverageServiceImpl implements AverageService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -29,13 +28,13 @@ public class AverageServiceImpl implements AverageService {
         }
 
         double result = -1;
-        SoapClient client = new SoapClient();
-        try {
-            result = client.calculateAverage(array);
-        } catch (ru.ilia.soap.client.exception.SumLessZeroException e) {
-            throw new SumLessZeroException();
-        }
-
+//        SoapClient client = new SoapClient();
+//        try {
+//            result = client.calculateAverage(array);
+//        } catch (ru.ilia.soap.client.exception.SumLessZeroException e) {
+//            throw new SumLessZeroException();
+//        }
+        result=234;
         return result;
     }
 }
